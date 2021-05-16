ce3cf9ad86ea6f82d7a277e081890e1db5550b35
Marcus <marcusbfs@gmail.com>
2021-05-16 14:58:34 -0300

    added pyinstaller depedency

--- scripts/make_exe.py
+++ scripts/make_exe.py
@@ -2,15 +2,23 @@ import os
 import pathlib
 import subprocess
 import time
+from pathlib import Path
 
 main_base = "cesp.py"
 script_dir = pathlib.Path(__file__).parent.absolute()
 repo_dir = os.path.abspath(os.path.join(script_dir, ".."))
 main_py = os.path.join(repo_dir, main_base)
 env_dir = "cesp_venv"
-python_exe = os.path.join(repo_dir, env_dir, "Scripts", "python.exe")
+python_exe = Path(os.path.join(repo_dir, env_dir, "Scripts", "python.exe"))
 
-cmd = [python_exe, "-m", "PyInstaller", "-F", "-c", main_py]
+if not python_exe.is_file():
+    env_dir = "env"
+    python_exe = Path(os.path.join(repo_dir, env_dir, "Scripts", "python.exe"))
+
+if not python_exe.is_file():
+    raise RuntimeError("Could not find virtual env folder")
+
+cmd = [str(python_exe), "-m", "PyInstaller", "-F", "-c", main_py]
 
 start_time = time.time()
 subprocess.call(cmd, shell=True)
