dd9e6a554a74143d67c672791ed3a12af7c7d3ee
Marcus <marcusbfs@gmail.com>
2021-05-21 18:45:47 -0300

    add more tests

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
-.PHONY = fmt t tn
+.PHONY:fmt t tn tests
 
-all: fmt
+all: fmt tests
 
 fmt:
 	autoflake --remove-all-unused-imports --remove-unused-variables --in-place cesp.py scripts/make_exe.py
 	isort cesp.py scripts/make_exe.py
 	black cesp.py scripts/make_exe.py
 
+
+tests:
+	pytest --cov cesp tests
 
 t: setup_test
 	python .\cesp.py -rdubscall test_folder
--- tests/test_cesp.py
+++ tests/test_cesp.py
@@ -1,4 +1,9 @@
 import pytest
+import os
+from pathlib import Path
+import shutil
+
+import py7zr
 
 import cesp
 
@@ -7,11 +12,30 @@ cesper.setDots(True)
 cesper.setSpecialChars(True)
 cesper.setBrackets(True)
 cesper.setUTF(True)
+cesper.setNoChange(True)
+
+
+CURRENT_DIR: Path = Path(__file__).parent.resolve()
 
 
 def test_object_creation() -> None:
     assert cesper is not None
     assert type(cesper) == cesp.cesp
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prepare_test_folder() -> None:  # type: ignore
+    test_folder_7z = CURRENT_DIR / "test_folder.7z"
+    test_folder: Path = test_folder_7z.with_name("test_folder")
+
+    test_folder.mkdir(exist_ok=True)
+
+    with py7zr.SevenZipFile(test_folder_7z, mode="r") as z:
+        z.extractall(path=CURRENT_DIR)
+
+    yield
+
+    shutil.rmtree(test_folder, ignore_errors=True)
 
 
 @pytest.mark.parametrize(
@@ -30,3 +54,59 @@ def test_rename_function(test_input: str, expected: str) -> None:
 
 def test_get_version() -> None:
     assert cesp.__version__ == cesper.getVersion()
+
+
+def test_fetching_files_no_recursive() -> None:
+    cesper.setPath(str(CURRENT_DIR / "test_folder"))
+    cesper.setChange(cesp.ChangeItemMode.files)
+    cesper.setRecursive(False)
+    og, ren = cesper.fetch()
+    assert len(og) == len(ren) == 1
+
+
+def test_fetching_files_recursive() -> None:
+    cesper.setPath(str(CURRENT_DIR / "test_folder"))
+    cesper.setRecursive(True)
+    cesper.setChange(cesp.ChangeItemMode.files)
+    og, ren = cesper.fetch()
+    assert len(og) == len(ren) == 5
+
+
+def test_fetching_dirs_no_recursive() -> None:
+    cesper.setPath(str(CURRENT_DIR / "test_folder"))
+    cesper.setChange(cesp.ChangeItemMode.dirs)
+    cesper.setRecursive(False)
+    og, ren = cesper.fetch()
+    assert len(og) == len(ren) == 2
+
+
+def test_fetching_dirs_recursive() -> None:
+    cesper.setPath(str(CURRENT_DIR / "test_folder"))
+    cesper.setRecursive(True)
+    cesper.setChange(cesp.ChangeItemMode.dirs)
+    og, ren = cesper.fetch()
+    assert len(og) == len(ren) == 2
+
+
+def test_fetching_all_no_recursive() -> None:
+    cesper.setPath(str(CURRENT_DIR / "test_folder"))
+    cesper.setChange(cesp.ChangeItemMode.all)
+    cesper.setRecursive(False)
+    og, ren = cesper.fetch()
+    assert len(og) == len(ren) == 3
+
+
+def test_fetching_all_recursive() -> None:
+    cesper.setPath(str(CURRENT_DIR / "test_folder"))
+    cesper.setRecursive(True)
+    cesper.setChange(cesp.ChangeItemMode.all)
+    og, ren = cesper.fetch()
+    assert len(og) == len(ren) == 7
+
+
+@pytest.mark.parametrize("name, expected", [
+    ('foo with spaces', 'foo_with_spaces'),
+    ('foo ç with spaces', 'foo_ç_with_spaces'),
+])
+def test_remove_blank_spaces(name:str, expected:str) -> None:
+    assert cesper._removeBlankSpaces(name) == expected
